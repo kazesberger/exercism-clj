3be1f70766bdddb565420fc75b11d41bec4912d2
kazesberger <klaus.azesberger@gmail.com>
2018-09-19 22:12:46 +0200

    Finish phone-number

--- phone-number/src/phone_number.clj
+++ phone-number/src/phone_number.clj
@@ -1,31 +1,31 @@
 (ns phone-number)
 
+; my first attempt to make use of defrecord.
+; kinda try-hard-apply this - bet there are way better solutions to this puz
+
 (defrecord Phonenumber [country area exchange subscriber])
+
 (defn phonenumber [string]
   (let [digits (filter (set (map char (range 48 58))) (seq string))
         country (when (= (count digits) 11) (first digits))
         digits-no-country (cond
-                            (< (count digits) 10) (str apply (repeat 10 \0))
-                            (and (= (count digits) 11) (not (= \1 (first digits)))) (str apply (repeat 10 \0))
-                            (> (count digits) 10) (str apply (repeat 10 \0))
-                            (:default) (take-last 10 digits))
+                            (< (count digits) 10) (apply str (repeat 10 \0))
+                            (and (= (count digits) 11) (not (= \1 (first digits)))) (apply str (repeat 10 \0))
+                            (> (count digits) 11) (apply str (repeat 10 \0))
+                            :default (take-last 10 digits))
         area (take 3 digits-no-country)
         exchange (take 3 (drop 3 digits-no-country))
         subscriber (take-last 4 digits-no-country)]
+    (println digits)
+    (println country)
+    (println digits-no-country)
+    (println area)
+    (println exchange)
+    (println subscriber)
     (map->Phonenumber {:country country
                        :area area
                        :exchange exchange
                        :subscriber subscriber})))
-
-(def sample-pn "+1 (613)-995-0253")
-
-(:exchange (phonenumber sample-pn))
-
-(let [pn (phonenumber sample-pn)]
-  (concat
-         (:area pn)
-         (:exchange pn)
-         (:subscriber pn)))
 
 (defn number [num-string] ;; <- arglist goes here
     (let [pn (phonenumber num-string)]
@@ -44,7 +44,3 @@
 (defn pretty-print [num-string] ;; <- arglist goes here
   (let [pn (phonenumber num-string)]
     (apply str (concat "(" (:area pn) ") " (:exchange pn) "-" (:subscriber pn)))))
-
-
-
-
